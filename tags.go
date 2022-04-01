d523d763c5cfb78d2ac346bc4e5d1c3a136bcfa0
Rob Thorne <rob@torenware.com>
2022-04-01 10:53:21 -0700

    Make sure that the URLs generated for assets are absolute and not relative.
    TODO: not sure how best to allow for apps run from sub-directories of web root.

--- tags.go
+++ tags.go
@@ -7,12 +7,12 @@ import (
 
 func (vg *VueGlue) RenderTags() (template.HTML, error) {
 	tags := `
-	<script type="module" crossorigin src="{{ .MainModule }}"></script>
+	<script type="module" crossorigin src="/{{ .MainModule }}"></script>
 	{{ range .Imports }}
-	<link rel="modulepreload" href="{{.}}">
+	<link rel="modulepreload" href="/{{.}}">
 	{{ end }}
 	{{ range .CSSModule }}
-	<link rel="stylesheet" href="{{.}}">
+	<link rel="stylesheet" href="/{{.}}">
 	{{ end }}
 	`
 	tmpl, err := template.New("tags").Parse(tags)
