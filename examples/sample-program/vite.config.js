771368ee20bc2f1fa4e2237a3ac42998d115edef
Rob Thorne <rob@torenware.com>
2022-04-05 11:44:34 -0700

    fixed formatting of sample vite.config.js file

--- examples/sample-program/vite.config.js
+++ examples/sample-program/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-		outDir: '../dist',
+    outDir: '../dist',
     sourcemap: true,
     manifest: true,
     rollupOptions: {
